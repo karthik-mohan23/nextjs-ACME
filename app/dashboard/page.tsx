9f2d68411d42f7fe67e477063680a3369930dd3a
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-07-29 16:01:16 +0530

    learned about static and dynamic rendering in Next.js.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
+
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
